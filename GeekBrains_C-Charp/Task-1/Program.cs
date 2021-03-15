7ed1ca1bb67f6cda386abe1796ea3bb190603299
Dmitry <abmingolbyoutude@gmail.com>
2021-03-15 15:45:42 +0300

    Task-1

---
+++ GeekBrains_C-Charp/Task-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}, today is {DateTime.Now:d}.");
+        }
+    }
+}
