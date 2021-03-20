4bd186f7e3677e6c3abdaf479809259b82e3820b
Dmitry <abmingolbyoutude@gmail.com>
2021-03-20 18:44:24 +0300

    task-2-3

---
+++ GeekBrains_C-Charp/task-2-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter any number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("Even number!");
+                }
+                else
+                {
+                    Console.WriteLine("Odd number!");
+                }
+            }
+        }
+    }
+}
