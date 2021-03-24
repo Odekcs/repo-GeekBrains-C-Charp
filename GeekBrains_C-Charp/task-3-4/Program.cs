873c5a047de1020692ea66e2eed246112da1c92b
Dmitry <abmingolbyoutude@gmail.com>
2021-03-24 20:15:02 +0300

    task-3-4

---
+++ GeekBrains_C-Charp/task-3-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] War =
+            {
+                {'O','X','X','X','O','O','O','O','O','O'},
+                {'O','O','O','O','O','O','O','X','X','X'},
+                {'X','O','O','O','O','X','O','O','O','O'},
+                {'O','O','X','O','O','O','O','O','O','O'},
+                {'O','O','X','O','O','O','O','O','X','X'},
+                {'O','O','X','O','O','X','O','O','O','O'},
+                {'X','O','X','O','O','O','O','O','O','O'},
+                {'X','O','O','O','O','O','O','O','O','O'},
+                {'O','O','O','O','O','O','O','X','O','O'},
+                {'X','X','O','O','O','O','O','O','O','O'}
+            };
+
+            for (int i = 0; i < War.GetLength(0); i++)
+            {
+                for (int j = 0; j < War.GetLength(1); j++)
+                {
+                    Console.Write(War[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
