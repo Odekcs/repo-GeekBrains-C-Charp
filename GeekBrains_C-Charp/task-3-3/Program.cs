681b8076a77c2d8c57d303e63d742260c45d27c9
Dmitry <abmingolbyoutude@gmail.com>
2021-03-24 20:13:13 +0300

    task-3-3

---
+++ GeekBrains_C-Charp/task-3-3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string text = Console.ReadLine();
+            string word = "";
+
+            for (int i = text.Length - 1; i >= 0; --i)
+            {
+                word += text[i];
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
