5362d3028901a08e9a135587d68737624f8d9468
Dmitry <abmingolbyoutude@gmail.com>
2021-03-24 20:12:14 +0300

    task-3-2

---
+++ GeekBrains_C-Charp/task-3-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] book =
+            {
+                {"Dmitry", "8 999 999 99 99"},
+                {"Dmitry", "8 999 999 99 99"},
+                {"Dmitry", "8 999 999 99 99"},
+                {"Dmitry", "8 999 999 99 99"},
+                {"Dmitry", "8 999 999 99 99"}
+            };
+
+            for (int i = 0; i < book.GetLength(0); i++)
+            {
+                for (int j = 0; j < book.GetLength(1); j++)
+                {
+                    Console.Write("{0,-15}", book[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
