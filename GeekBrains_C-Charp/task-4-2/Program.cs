7f3e96a9c1c8a7b1f416957d11a927df0043121c
Dmitry <abmingolbyoutude@gmail.com>
2021-03-28 16:48:15 +0300

    task-4-2

---
+++ GeekBrains_C-Charp/task-4-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int totalsumm;
+
+                Console.Write("Enter numbers separated by a space: ");
+                string strNumbers = Console.ReadLine();
+
+                totalsumm = SummNumbersStr(strNumbers);
+                Console.WriteLine(totalsumm);
+            }
+        }
+        static int SummNumbersStr(string str)
+        {
+            int summ = 0;
+            string[] strNumbers = str.Split(new char[] { ' ' });
+
+            for (int i = 0; i < strNumbers.Length; i++)
+            {
+                if (char.IsNumber(str[i]))
+                {
+
+                    summ += Convert.ToInt32(strNumbers[i]);
+                }
+            }
+            return summ;
+        }
+    }
+}
